146b3386b4fdaee6f941cb5a6535619b4c5eebcf
Pavel Terekhov <pavel.a.terekhov@gmail.com>
2023-07-08 01:14:30 +0700

    task Pug

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,1 +1,20 @@
-const gulp = require("gulp")
+import gulp from "gulp";
+import gpug from "gulp-pug";
+import {deleteSync} from 'del'
+
+const routes = {
+    pug: {
+        src: "src/*.pug",
+        dest: "build"
+    }
+};
+
+const pug = () => 
+    gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+
+const clear = () => deleteSync(["build"]);
+
+const prepare = gulp.series([clear]);
+const assets = gulp.series([pug]);
+
+export const dev = gulp.series([prepare, assets]);
