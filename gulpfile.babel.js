5c34547807196bb7bc91f0598982736c57120fa7
Pavel Terekhov <pavel.a.terekhov@gmail.com>
2023-07-09 22:56:58 +0700

    gulp-webserver

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,20 +1,33 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
-import {deleteSync} from 'del'
+import {deleteSync} from "del";
+import ws from "gulp-webserver";
 
 const routes = {
     pug: {
+        watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
     }
 };
 
 const pug = () => 
-    gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+    gulp
+        .src(routes.pug.src)
+        .pipe(gpug())
+        .pipe(gulp.dest(routes.pug.dest));
 
 const clear = () => deleteSync(["build"]);
-
+const webserver = () => {
+    gulp
+        .src("build")
+        .pipe(ws({livereload: true, open: true}));
+}
+const watch = () => {
+    gulp.watch(routes.pug.watch, pug);
+}
 const prepare = gulp.series([clear]);
 const assets = gulp.series([pug]);
+const postDev = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets]);
+export const dev = gulp.series([prepare, assets, postDev]);
