31cbc8f8dbcbd13f6c7dcebdadc327b722250fee
Pavel Terekhov <pavel.a.terekhov@gmail.com>
2023-07-11 12:52:14 +0700

    adedd gulp-image

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,32 +1,42 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
+const gulpWebserver = require('gulp-webserver');
 import {deleteSync} from "del";
-import ws from "gulp-webserver";
+import image from "gulp-image";
+
 
 const routes = {
     pug: {
         watch: "src/**/*.pug",
         src: "src/*.pug",
         dest: "build"
+    },
+    img: {
+        src: 'src/img/*',
+        dest: 'build/img'
     }
 };
 
-const pug = () => 
-    gulp
-        .src(routes.pug.src)
-        .pipe(gpug())
-        .pipe(gulp.dest(routes.pug.dest));
+const pug = () => {
+    gulp.src(routes.pug.src).pipe(gpug()).pipe(gulp.dest(routes.pug.dest));
+};
 
 const clear = () => deleteSync(["build"]);
+
 const webserver = () => {
-    gulp
-        .src("build")
-        .pipe(ws({livereload: true, open: true}));
-}
+  gulp.src("build").pipe(gulpWebserver({livereload: true, open: true }));
+};
+
 const watch = () => {
-    gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.pug.watch, pug);
+  gulp.watch(routes.img.src, img);
+};
+
+const img = () => {
+    gulp.src(routes.img.src).pipe(image()).pipe(gulp.dest(routes.img.dest));
 }
-const prepare = gulp.series([clear]);
+
+const prepare = gulp.series([clear, img]);
 const assets = gulp.series([pug]);
 const postDev = gulp.parallel([webserver, watch]);
 
